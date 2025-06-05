597556e1c97e1f208f2d82f1e0f01d952a344019
clz2025-red <clz2025.red@gmail.com>
2025-06-05 11:57:36 +0900

    2025-06-05

--- src/com/javaex/ex03/AuthorDAO.java
+++ src/com/javaex/ex03/AuthorDAO.java
@@ -24,7 +24,6 @@ public class AuthorDAO {
 		// 0. import java.sql.*;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		try {
 			// 1. JDBC 드라이버 (MySQL) 로딩
@@ -60,9 +59,6 @@ public class AuthorDAO {
 
 			// 5. 자원정리
 			try {
-				if (rs != null) {
-					rs.close();
-				}
 				if (pstmt != null) {
 					pstmt.close();
 				}
@@ -81,7 +77,54 @@ public class AuthorDAO {
 
 	// 작가 삭제
 	public void authorDelete(int authorId) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
+		try {
+			// 1. JDBC 드라이버 (MySQL) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/web_db";
+			conn = DriverManager.getConnection(url, "web", "web");
+		
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " delete from author ";
+			query += " where author_id = ? ";
+			System.out.println(query);
+			
+			// 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, authorId);
+			
+			// 실행
+			int count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			System.out.println(count + "건이 삭제 되었습니다.");
+		
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+		
+		}
+
 		
 	} // delete()
 	
